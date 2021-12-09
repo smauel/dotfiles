48c1892f9e2b64488edb212685d54c20610d53b2
Sam Morrison <samuelmorrison87@gmail.com>
2021-12-09 21:36:39 +0000

    update nvimtree config

--- nvim/lua/config/nvimtree.lua
+++ nvim/lua/config/nvimtree.lua
@@ -1,7 +1,8 @@
 local g = vim.g
 
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 1
 g.nvim_tree_quit_on_open = 1
-g.nvim_tree_gitignore = 1
-g.nvim_tree_disable_netrw = 0
+
+require'nvim-tree'.setup {
+  disable_netrw       = false,
+  auto_close          = true,
+}
