b633d93c802bfe6237138d83beb9958e93bd1240
Sam Morrison <samuelmorrison87@gmail.com>
2021-12-21 12:45:58 +0000

    (refactor): lualine

---
+++ config/nvim/lua/config/lualine/init.lua
@@ -0,0 +1,34 @@
+local components = require('config.lualine.components')
+
+local default_options = {
+  icons_enabled = true,
+  component_separators = { left = "", right = "" },
+  section_separators = { left = "", right = "" },
+  disabled_filetypes = { 'NvimTree' },
+}
+
+require('lualine').setup {
+  options = default_options,
+  sections = {
+    lualine_a = {
+      components.mode
+    },
+    lualine_b = {
+      components.branch,
+      components.filename
+    },
+    lualine_c = {
+      components.diff
+    },
+    lualine_x = {
+      components.diagnostics,
+      components.treesitter,
+      components.filetype
+    },
+    lualine_y = {},
+    lualine_z = {
+      components.progress
+    }
+  }
+}
+
