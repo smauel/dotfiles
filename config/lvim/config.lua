f333116e51f10dae1e399ec9eab54bd2ad768d3e
Sam Morrison <sam.morrison@ef.com>
2022-07-31 01:11:04 +0100

    lsp

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -279,63 +279,44 @@ lvim.builtin.treesitter.ensure_installed = {
 }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- lsp
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
--- vim.tbl_map(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
-
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+-- null-ls
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup {
+  { name = "eslint_d" },
+  { name = "gitsigns" },
+  { name = "refactoring" },
+  { name = "shellcheck" },
+}
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { name = "codespell" },
+  { name = "actionlint" },
+  { name = "checkmake" },
+  { name = "eslint_d" },
+  { name = "gitlint" },
+  { name = "golangci_lint" },
+  { name = "hadolint" },
+  { name = "jsonlint" },
+  { name = "luacheck" },
+  { name = "markdownlint" },
+  { name = "shellcheck" },
+  { name = "yamllint" },
+  { command = "sqlfluff", extra_args = { "--dialect", "postgres" } },
+}
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "codespell" },
+  { name = "eslint_d" },
+  { name = "fixjson" },
+  { name = "gofmt" },
+  { name = "goimports" },
+  { name = "markdownlint" },
+  { name = "prettierd" },
+  { name = "terraform_fmt" },
+  { name = "trim_newlines" },
+  { name = "trim_whitespace" },
+  { command = "sqlfluff", extra_args = { "--dialect", "postgres" } },
+}
 
 -- plugins
 lvim.plugins = {
@@ -347,10 +328,6 @@ lvim.plugins = {
   {
     "tpope/vim-surround",
     keys = { "c", "d", "y" }
-    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-    -- setup = function()
-    --  vim.o.timeoutlen = 500
-    -- end
   },
   { "tpope/vim-repeat" },
   {
@@ -371,7 +348,7 @@ lvim.plugins = {
   },
 }
 
--- autocmd
+-- autocmds
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*.json", "*.jsonc" },
   -- enable wrap mode for json files only
