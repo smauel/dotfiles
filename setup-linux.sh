59eec39951de050715054fd4243e27382fa1217a
Sam Morrison <samuelmorrison@gmail.com>
2024-07-20 13:13:24 +0100

    setup: add some install steps to linux setup

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,3 +1,20 @@
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ ! -d ~/.fzf ]; then
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
+fi
+
+if [ ! -d ~/.sdkman ]; then
+	curl -s "https://get.sdkman.io" | bash
+fi
+
+if [ ! -d ~/.nvm ]; then
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
+
 stow -t ~ zsh
 stow -t ~ alacritty
 stow -t ~ tmux
