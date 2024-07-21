b91fccd872238fce718867f61da5f0a2b204c3a2
Sam Morrison <samuelmorrison@gmail.com>
2024-07-21 21:12:59 +0100

    repos: add repos config to dotfiles

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,18 +1,18 @@
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 if [ ! -d ~/.fzf ]; then
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
 fi
 
 if [ ! -d ~/.sdkman ]; then
-	curl -s "https://get.sdkman.io" | bash
+  curl -s "https://get.sdkman.io" | bash
 fi
 
 if [ ! -d ~/.nvm ]; then
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 fi
 
 stow -t ~ zsh
@@ -22,3 +22,4 @@ stow -t ~ nvim
 stow -t ~ bin
 stow -t ~ git
 stow -t ~ ripgrep
+stow -t ~ repos
