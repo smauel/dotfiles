3dc187434c3b190ddd986c4348b5b79feab12b55
Sam Morrison <samuelmorrison87@gmail.com>
2024-12-09 00:34:30 +0000

    tmux: change install dir for tpm

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
 stow -t ~ bin
 stow -t ~ home
 stow -t ~/.config config
+
+if [ ! -d ~/.config/tmux/plugins/tpm/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
